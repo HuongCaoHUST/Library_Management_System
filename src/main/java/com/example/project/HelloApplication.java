210e28ad7df873fe5b056394840ba87d22292dbb
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-10-08 10:00:57 +0700

    First commit

---
+++ src/main/java/com/example/project/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.example.project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_form.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 500);
+        stage.setTitle("HỆ THỐNG QUẢN LÝ THƯ VIỆN");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
