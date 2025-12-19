26a88fc9f0e1f8e02b9a95dd83f0744c538b4074
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-12-19 14:09:09 +0700

    small fix

--- src/main/java/com/example/project/dto/response/RoleResponse.java
+++ src/main/java/com/example/project/dto/response/RoleResponse.java
@@ -1,21 +1,25 @@
 package com.example.project.dto.response;
 
-import com.example.project.model.Reader;
 import com.example.project.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoleResponse {
-    private String rollName;
+    private String roleName;
     private String description;
+    private List<PermissionResponse> permissions;
+
     public RoleResponse (Role role) {
-        this.rollName = role.getName();
+        this.roleName = role.getName();
         this.description = role.getDescription();
+        this.permissions = role.getPermissions().stream().map(PermissionResponse::new).toList();;
     }
 }
