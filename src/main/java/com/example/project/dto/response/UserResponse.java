a3a3828b72ccb99c449d553acf5815a98545e200
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-12-29 10:41:41 +0700

    fix contructor

--- src/main/java/com/example/project/dto/response/UserResponse.java
+++ src/main/java/com/example/project/dto/response/UserResponse.java
@@ -1,9 +1,13 @@
 package com.example.project.dto.response;
 
 import com.example.project.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class UserResponse {
 
     private final Long userId;
@@ -12,14 +16,5 @@ public class UserResponse {
     private final String email;
     private final String role;
     private final String status;
-
-    public UserResponse(User user) {
-        this.userId = user.getUserId();
-        this.fullName = user.getFullName();
-        this.username = user.getUsername();
-        this.email = user.getEmail();
-        this.role = user.getRole().getName();
-        this.status = user.getStatus();
-    }
 }
 
