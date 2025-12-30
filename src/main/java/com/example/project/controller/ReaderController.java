ec8095c236e6dc853fa316b6c7387ebc281fd7ac
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-12-30 15:57:18 +0700

    small fix

--- src/main/java/com/example/project/controller/ReaderController.java
+++ src/main/java/com/example/project/controller/ReaderController.java
@@ -125,7 +125,7 @@ public class ReaderController {
 
     @PostMapping(value = "/register-with-avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<ReaderResponse>> registerWithAvatar(
-            @RequestPart("data") ReaderRequest request, // sửa từ @RequestBody
+            @RequestPart("data") ReaderRequest request,
             @RequestPart(value = "file", required = false) MultipartFile file
     ) {
         try {
